f017c0c2d559e6c60594f6b40b96535aed66ce17
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-02-18 09:29:00 +0800

    add timezone

--- xmas.py
+++ xmas.py
@@ -1,20 +1,21 @@
-from datetime import datetime, date
-from inspect import getfile
+from datetime import datetime, date, timedelta, timezone, tzinfo
 from PIL import Image, ImageFont, ImageDraw
+timeDiff = 8
+tz = timezone(timedelta(hours=timeDiff))
 
 
 def getDiff():
-    currentTime = datetime.today()
-    xmasTime = datetime(currentTime.year, 12, 25)
+    currentTime = datetime.now(tz)
+    xmasTime = datetime(currentTime.year, 12, 25, 0, 0, 0, tzinfo=tz)
     if(currentTime > xmasTime):
-        xmasTime = datetime(currentTime.year+1, 12, 25, 0, 0, 0)
+        xmasTime = datetime(currentTime.year+1, 12, 25, 0, 0, 0, tzinfo=tz)
     diff = (xmasTime - currentTime)
     print(diff)
     return diff
 
 
 def drawOnImg(daysDiff):
-    text = f'Next xmas : {daysDiff.days} days, {daysDiff.seconds//3600} hours, {(daysDiff.seconds//60)%60} minutes'
+    text = f'Next xmas : {daysDiff.days} days, {daysDiff.seconds//3600} hours, {(daysDiff.seconds//60)%60} minutes {daysDiff.seconds%60} seconds'
     img = Image.open("./img/padoru.jpg")
     drawing = ImageDraw.Draw(img)
     font = ImageFont.truetype(
